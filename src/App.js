6eeeef93771fa378b26c95ba1cb1838b6e729b4a
이재훈 <jami8291215@gmail.com>
2021-01-14 23:44:09 +0900

    :zap: .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // css
+import { useState } from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.scss';
+import { initialState } from './assets/state';
 
 // components
 import Nav from "./components/Nav";
@@ -8,16 +10,20 @@ import ItemListContainer from "./pages/ItemListContainer";
 import ShoppingCart from "./pages/ShoppingCart";
 
 function App() {
+  // console.log(initialState)
+  const [items, setItems] = useState(initialState.items)
+  const [cartItems, setCartItems] = useState(initialState.cartItems)
+
   return (
     <div className="App">
       <Router>
         <Nav />
         <Switch>
           <Route exact={true} path="/">
-            <ItemListContainer />
+            <ItemListContainer items={items} />
           </Route>
           <Route path="/shoppingcart">
-            <ShoppingCart />
+            <ShoppingCart items={items} cartItems={cartItems} />
           </Route>
         </Switch>
       </Router>
---
+++ src/assets/state.js
@@ -0,0 +1,85 @@
+export const initialState =
+{
+  "items": [
+    {
+      "id": 1,
+      "name": "Apple Iphone 12",
+      "img": "../images/Apple Iphone 12.jpeg",
+      "price": 9900
+    },
+    {
+      "id": 2,
+      "name": "MacBook Air",
+      "img": "../images/MacBook Air.jpeg",
+      "price": 9900
+    },
+    {
+      "id": 3,
+      "name": "갤럭시 s21",
+      "img": "../images/갤럭시 s21.jpeg",
+      "price": 9900
+    },
+    {
+      "id": 4,
+      "name": "갤럭시 노트 20",
+      "img": "../images/갤럭시 노트 20.jpeg",
+      "price": 9900
+    },
+    {
+      "id": 5,
+      "name": "갤럭시 탭 s7",
+      "img": "../images/갤럭시 탭 s7.jpeg",
+      "price": 9900
+    },
+    {
+      "id": 6,
+      "name": "갤럭시패드 2020",
+      "img": "../images/갤럭시패드 2020.jpeg",
+      "price": 9900
+    },
+    {
+      "id": 7,
+      "name": "레노버 ThinkPad",
+      "img": "../images/레노버 ThinkPad.jpeg",
+      "price": 9900
+    },
+    {
+      "id": 8,
+      "name": "삼성 노트북 7",
+      "img": "../images/삼성 노트북 7.jpeg",
+      "price": 9900
+    },
+    {
+      "id": 9,
+      "name": "아이패드 7세대",
+      "img": "../images/아이패드 7세대.jpeg",
+      "price": 9900
+    },
+    {
+      "id": 10,
+      "name": "아이패드프로4",
+      "img": "../images/아이패드프로4.jpeg",
+      "price": 9900
+    },
+    {
+      "id": 11,
+      "name": "아이폰 로즈 골드 7",
+      "img": "../images/아이폰 로즈 골드 7.jpeg",
+      "price": 9900
+    },
+  ],
+  "cartItems": [
+    {
+      "itemId": 1,
+      "quantity": 1
+    },
+    {
+      "itemId": 5,
+      "quantity": 7
+    },
+    {
+      "itemId": 2,
+      "quantity": 3
+    },
+  ]
+}
