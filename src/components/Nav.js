26ea798f44f2adfce84636c6b886f505673ba049
이재훈 <jami8291215@gmail.com>
2021-01-15 12:58:06 +0900

    :zap: nav css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 // css
 import "../css/Nav.scss";
 
 function Nav() {
+  const [count, setCount] = useState(0);
   return (
     <div id="nav">
       <span id="title">
@@ -12,7 +13,9 @@ function Nav() {
       </span>
       <div id="menu">
         <NavLink to="/" className="item_list_container">상품리스트</NavLink>
-        <NavLink to="/shoppingcart" className="item_list_container">장바구니</NavLink>
+        <NavLink to="/shoppingcart" className="item_list_container">
+          장바구니 <span id="nav-item-counter"> {count} </span>
+        </NavLink>
       </div>
     </div>
   )
